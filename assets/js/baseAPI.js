e3a3baee91568d891641b2d795b45ae33e6cc99c
lzd-Megatron <77382122+lzd-Megatron@users.noreply.github.com>
2021-01-17 17:03:39 +0800

    finish index page

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,8 +1,29 @@
-$(function(){
+$(function () {
     // 该方法为jq提供,在每次发送ajax请求之前会被自动调用,用于拦截发送的ajax请求
     // options为发送的ajax中传入的配置对象,可在该函数中对ajax请求的配置对象进行一些处理或统一管理
-    $.ajaxPrefilter(function(options){
+    $.ajaxPrefilter(function (options) {
         options.url = 'http://api-breakingnews-web.itheima.net' + options.url
+
+        //以 /my/开头的请求路径，需要在请求头中携带 Authorization 身份认证字段，才能正常访问成功
+        if (options.url.indexOf('/my/') !== -1) {
+            // 根据文档要求为请求配置对象options添加headers对象属性,属性名为Authorization,值为localStorage中的token
+            options.headers = {
+                Authorization: localStorage.getItem('token') || ''
+            }
+        }
+
+        //统一为每次发送的ajax请求的配置对象挂在complete()函数,验证用户权限
+        options.complete = function (res) {
+            // console.log(res);
+            // 打印服务器返回的数据决定这里怎么写
+            if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+                //重新跳转回登录页
+                location.href = 'login.html'
+                //清空token权限令牌
+                localStorage.removeItem('token')
+            }
+        }
+
     })
 
 
---
+++ assets/js/index.js
@@ -0,0 +1,70 @@
+$(function(){
+    // 获取layui的layer对象
+    let layer = layui.layer
+
+    // 主页一打开就调用函数获取用户信息
+    getuserInfo (layer)
+
+    // 监听退出事件
+    $('#btnLogout').on('click',function(){
+        layer.confirm('确认退出吗?', {icon: 3, title:'温馨提示=V='}, function(index){
+            // 跳转到登录页
+            location.href = 'login.html'
+            //清空本地存储的token
+            localStorage.removeItem('token')
+            
+            //layui的layer confirm自带的语句用于关闭询问框
+            layer.close(index);
+          });
+    })
+})
+
+//封装获取用户信息的函数
+function getuserInfo (layer){
+    
+    $.ajax({
+        method:'get',
+        url:'/my/userinfo',
+        // headers就是请求头配置对象(根据文档要求传入token令牌内容获取访问用户信息权限)
+        //在baseAPI.js中统一设置headers请求头,避免在多次访问需要权限接口时频繁设置请求头
+        // headers:{
+        //     Authorization: localStorage.getItem('token') || ''
+        // },
+        success:function(res){
+            if(res.status !== 0){
+                return layer.msg(res.message)
+            }
+            layer.msg('欢迎进入主页')
+            //请求成功则调用渲染用户信息的函数
+            renderUserInfo(res.data)
+        },
+        //控制用户权限,不经过登录直接通过输入url进入主页没得到正确的服务器返回信息则重新
+        // 将页面返回至登录页
+        //complete:function(res){
+        //    // console.log(res);
+        //    // 打印服务器返回的数据决定这里怎么写
+        //    if(res.responseJSON.status ===1 && res.responseJSON.message === '身份认证失败！'){
+        //        //重新跳转回登录页
+        //        location.href = 'login.html'
+        //        //清空token权限令牌
+        //        localStorage.removeItem('token')
+        //    }
+        //}
+    })
+}
+
+function renderUserInfo(data){
+    let name = data.nickname || data.username
+    let firstLetter = name[0].toUpperCase()
+    $('.welcome').html('欢迎&nbsp;&nbsp;'+name)
+    if(data.user_pic !== null){
+        // 渲染图片头像
+        $('.layui-nav-img').attr('src','data.user_pic').show()
+        $('.user-img').hide()
+    }else{
+        //渲染文本头像
+        $('.layui-nav-img').hide()
+        $('.user-img').html(firstLetter).show()
+
+    }
+}
